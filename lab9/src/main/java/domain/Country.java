6d70914d20ca927fbf3ffd1989a6afdc26e959ac
lukyanov <serjluk4@gmail.com>
2017-10-29 21:55:45 +0300

    new1: country support for lab9 added

---
+++ lab9/src/main/java/domain/Country.java
@@ -0,0 +1,35 @@
+package domain;
+
+public class Country {
+
+    private int id;
+
+    private String name;
+
+    private String codeName;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCodeName() {
+        return codeName;
+    }
+
+    public void setCodeName(String codeName) {
+        this.codeName = codeName;
+    }
+}
---
+++ lab9/src/main/java/mvc/form/bean/CountryFormBean.java
@@ -0,0 +1,31 @@
+package mvc.form.bean;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class CountryFormBean {
+
+    @NotNull
+    @Size(min = 2, max = 30)
+    private String name;
+
+    @NotNull
+    @Size(min = 2, max = 30)
+    private String codeName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCodeName() {
+        return codeName;
+    }
+
+    public void setCodeName(String codeName) {
+        this.codeName = codeName;
+    }
+}
